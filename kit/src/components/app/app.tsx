577a9ab03ee0bafc588dba9ee70e3138d878327e
ChicK00o <hirohitthegreat@gmail.com>
2021-02-01 23:00:38 +0530

    adding error boundary

--- kit/src/components/app/app.tsx
+++ kit/src/components/app/app.tsx
@@ -1,18 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import Routes from '../../routes';
+import { ErrorBoundary } from '../../utils/error-boundary';
 
-const app = () => {
-    return <Routes />;
+const App = () => {
+    return (
+        <ErrorBoundary>
+            <Routes />
+        </ErrorBoundary>
+    );
 };
 
-export default app;
+export default App;
