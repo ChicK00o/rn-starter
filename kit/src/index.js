eaec0e0aac35b40e29e133bf353b894d3a18893b
ChicK00o <hirohitthegreat@gmail.com>
2021-02-01 20:18:32 +0530

    adding mocker + changes

--- kit/src/index.js
+++ kit/src/index.js
@@ -1,10 +1,8 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import App from './components/app';
-import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {rootTag: document.getElementById('root')});
+AppRegistry.runApplication(appName, {
+    rootTag: document.getElementById('root'),
+});
